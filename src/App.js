15b0a139e38ed4a2803d428cd4d9a2835d0f764e
sujingyi <sochingyeess@gmail.com>
2023-01-29 19:14:54 +0800

    feat: 尝试引入web worker

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import DPlayer from "dplayer";
 
 
 import { useRef, useState, useEffect } from "react";
-import { Form, Radio, Switch, Slider, message } from "antd";
+import { Form, Radio, Switch, Slider, message, Spin } from "antd";
+
 
 
 
@@ -19,9 +20,11 @@ function App() {
 
 const [maskImageUrl,setMaskImageUrl] = useState('')
 const [isMaskOpen,setIsMaskOpen] = useState(true)
+const [loading,setLoading] = useState(true)
 
 const dplayer = useRef(null)
 const segmenter = useRef(null)
+const animation = useRef(null)
 
 useEffect(() => {
 
@@ -30,12 +33,14 @@ useEffect(() => {
 
 }, [])
 
-const danmuLists = ["时 隔 四 年 依 然 经 典", "小小房间竟然人才辈出，实在是恐怖如斯", "《有生之年看到原版》", "以前的鲲鲲充满了自信和笑容", "梦开始的地方", "1.自我介绍", "这短暂的一分钟养活了多少up主（doge）", "万 恶 之 源", "《开 始 吟 唱》", "《名场面》", "《music》", "《经典咏流传》", "非物质文化遗产", "《文艺复兴》", "《白 带 异 常》", "梦开始的地方，我的梦回来了", "每一帧都是经典", "每一帧都是表情包诶", "《梦开始的地方》", "5.优雅转身", "前 方 高 能", "准备铁山靠", "《铁山靠》", "招式3:《铁 山 靠》", "坤法第一式：铁山靠", "小秘密：随便一帧都是经典画面", "《白鹤亮翅》", "跳水00:40", "《 老 肩 巨 滑 》", "《每天一遍，精神百倍》", "优雅，真的是优雅", "《经典永流传》", "《垂直握把》", "《起舞弄清影》", "每时每刻打开都有人在看系列", "《护裆派》", "《让我蠢蠢欲动》", "᭙ᦔꪀꪑᦔ，​", "这种感觉我从未有Cause I Got A Cush On You Who You", "武当（档）派", "爷不知道就他笑死了多少无辜的生命", "《国家宝藏》之家禽", "《无效定语从句》", "卧槽，每一帧都是万恶之源", "“鳖”", "每日一遍", "嘎子偷狗时代考察", "《亡之音》", "看见这个头型我就想笑", "最后亿遍", "期待的话，请多多为我投票吧！", "《作词作曲》", "你干嘛∽哈哈～哎呦", "糖果超甜", "笑死我了哈哈哈哈", "你是我的我是你的谁", "原版都这么滑稽哈哈哈", "你不要过来！！！", "《啥时候都有人》", "我的口头禅"]
+const danmaku = ["时 隔 四 年 依 然 经 典", "小小房间竟然人才辈出，实在是恐怖如斯", "《有生之年看到原版》", "以前的鲲鲲充满了自信和笑容", "梦开始的地方", "1.自我介绍", "这短暂的一分钟养活了多少up主（doge）", "万 恶 之 源", "《开 始 吟 唱》", "《名场面》", "《music》", "《经典咏流传》", "非物质文化遗产", "《文艺复兴》", "《白 带 异 常》", "梦开始的地方，我的梦回来了", "每一帧都是经典", "每一帧都是表情包诶", "《梦开始的地方》", "5.优雅转身", "前 方 高 能", "准备铁山靠", "《铁山靠》", "招式3:《铁 山 靠》", "坤法第一式：铁山靠", "小秘密：随便一帧都是经典画面", "《白鹤亮翅》", "跳水00:40", "《 老 肩 巨 滑 》", "《每天一遍，精神百倍》", "优雅，真的是优雅", "《经典永流传》", "《垂直握把》", "《起舞弄清影》", "每时每刻打开都有人在看系列", "《护裆派》", "《让我蠢蠢欲动》", "᭙ᦔꪀꪑᦔ，​", "这种感觉我从未有Cause I Got A Cush On You Who You", "武当（档）派", "爷不知道就他笑死了多少无辜的生命", "《国家宝藏》之家禽", "《无效定语从句》", "卧槽，每一帧都是万恶之源", "“鳖”", "每日一遍", "嘎子偷狗时代考察", "《亡之音》", "看见这个头型我就想笑", "最后亿遍", "期待的话，请多多为我投票吧！", "《作词作曲》", "你干嘛∽哈哈～哎呦", "糖果超甜", "笑死我了哈哈哈哈", "你是我的我是你的谁", "原版都这么滑稽哈哈哈", "你不要过来！！！", "《啥时候都有人》", "我的口头禅"]
+
+
 
 // 加载模型
 async function bodySegmentationInit(){
   try{
-    message.info('模型加载中')
+
     const model = bodySegmentation.SupportedModels.MediaPipeSelfieSegmentation;
     const segmenterConfig = {
       runtime:'mediapipe',
@@ -48,9 +53,11 @@ async function bodySegmentationInit(){
     segmenter.current = await bodySegmentation.createSegmenter(model,segmenterConfig)
  
     dplayer.current?.notice('模型加载完成',3000)
+    setLoading(false)
   
   }catch(e){
     dplayer.current?.notice(`模型加载失败${e}`,3000)
+    setLoading(false)
   }
 }
 
@@ -60,21 +67,25 @@ async function bodySegmentationInit(){
     container:document.getElementById("container"),
     loop:true,
     video:{
-      url:require('./source/k.mp4'),
+      url: "https://dcdn.it120.cc/2022/11/04/87b7b5af-476c-4d2b-ae90-36724d66a407.mp4"
     },
 
   })
-  segmenter.current?.dispose()
+  await segmenter.current?.dispose() //从内存中释放基础模型。
+  await bodySegmentationInit()
  
 
   dplayer.current.on('play',async ()=>{
-    console.log('play');
+ 
   })
 
+  dplayer?.current?.on('pause',()=>{
+   
+  })
 }
 
-// 使用canvas描绘视频的每一帧
-function drawImageInCanvas(el){
+// canvas压缩视频的每一帧
+function compress(el){
   return new Promise(async resolve => {
   
     const canvas = document.createElement('canvas')
@@ -130,55 +141,73 @@ function imgLoad(src){
 
 // 识别
 async function recognition(){
-  const subtitles_container = segmenter.current && dplayer.current && dplayer.current.querySelector(".dplayer-danmaku")
-  try{
-    if(segmenter.current && isMaskOpen){
-      const canvas = document.createElement("canvas")
-      const context = canvas.getContext('2d')
 
-      // 压缩视频
-      const imageData = await drawImageInCanvas(dplayer.current.video)
+  // const subtitles_container = segmenter.current && dplayer.current && dplayer.current.querySelector(".dplayer-danmaku")
+  // try{
+  //   if(segmenter.current && isMaskOpen){
+  //     const canvas = document.createElement("canvas")
+  //     const context = canvas.getContext('2d')
+
+  //     // 压缩视频
+  //     const imageData = await compress(dplayer.current.video)
       
-      const segmentationConfig = {
-        flipHorizontal: false,
-        multiSegmentation: false,
-        segmentBodyParts: true,
-        segmentationThreshold: 1,
-      }
-      const people = await segmenter.current.segmentPeople(imageData,segmentationConfig)
+  //     const segmentationConfig = {
+  //       flipHorizontal: false,
+  //       multiSegmentation: false,
+  //       segmentBodyParts: true,
+  //       segmentationThreshold: 1,
+  //     }
+  //     const people = await segmenter.current.segmentPeople(imageData,segmentationConfig)
       
-      // 官网默认的配置
-      const foregroundColor = { r: 0, g: 0, b: 0, a: 0 }; //用于可视化属于人的像素的前景色 (r,g,b,a)。
-      const backgroundColor = { r: 0, g: 0, b: 0, a: 255 }; //用于可视化不属于人的像素的背景颜色 (r,g,b,a)。
-      const drawContour = false; //是否在每个人的分割蒙版周围绘制轮廓。
-      const foregroundThresholdProbability = 0.3
-      const backgroundDarkeningMask = await bodySegmentation.toBinaryMask(people,foregroundColor,backgroundColor,drawContour,foregroundThresholdProbability)
+  //     // 官网默认的配置
+  //     const foregroundColor = { r: 0, g: 0, b: 0, a: 0 }; //用于可视化属于人的像素的前景色 (r,g,b,a)。
+  //     const backgroundColor = { r: 0, g: 0, b: 0, a: 255 }; //用于可视化不属于人的像素的背景颜色 (r,g,b,a)。
+  //     const drawContour = false; //是否在每个人的分割蒙版周围绘制轮廓。
+  //     const foregroundThresholdProbability = 0.3
+  //     const backgroundDarkeningMask = await bodySegmentation.toBinaryMask(people,foregroundColor,backgroundColor,drawContour,foregroundThresholdProbability)
     
-      canvas.width = backgroundDarkeningMask.width
-      canvas.height = backgroundDarkeningMask.height
-      context.putImageData(backgroundDarkeningMask,0,0)
-      const base64Image = canvas.toDataURL("image/png") // 黑白蒙层
-      setMaskImageUrl(base64Image)
-      const { width, height } = dplayer.current.video.getBoundingClientRect()
-      //加载图片到缓存中（如果不加载到缓存中，会导致mask-image失效，因为图片还没有加载到页面上，新的图片已经添加上去了，会导致图片一直是个空白）
-      await imgLoad(base64Image)
-      subtitles_container.style = `-webkit-mask-image: url(${base64Image});-webkit-mask-size: ${width}px ${height}px;`
-      window.requestAnimationFrame(recognition)
-    }
-    else{
-      subtitles_container.style = ''
+  //     canvas.width = backgroundDarkeningMask.width
+  //     canvas.height = backgroundDarkeningMask.height
+  //     context.putImageData(backgroundDarkeningMask,0,0)
+  //     const base64Image = canvas.toDataURL("image/png") // 黑白蒙层
+  //     setMaskImageUrl(base64Image)
+  //     const { width, height } = dplayer.current.video.getBoundingClientRect()
+  //     //加载图片到缓存中（如果不加载到缓存中，会导致mask-image失效，因为图片还没有加载到页面上，新的图片已经添加上去了，会导致图片一直是个空白）
+  //     await imgLoad(base64Image)
+  //     subtitles_container.style = `-webkit-mask-image: url(${base64Image});-webkit-mask-size: ${width}px ${height}px;`
+  //     window.requestAnimationFrame(recognition)
+  //   }
+  //   else{
+  //     subtitles_container.style = ''
      
-    }
+  //   }
 
-  }catch(e){
-    message.error(e)
-  }
+  // }catch(e){
+  //   message.error(e)
+  // }
 }
-  
+
+
+// 绘制弹幕
+function randomDanmaku(){
+  const danmu = danmaku
+  const colors = ['#ffffff', '#fffb8f', '#5b8c00', '#d3adf7' , '#c41d7f' ,'#e54256', '#ffe133', '#64DD17', '#D500F9'];
+  const types = ['top','bottom', 'rigth']
+  const randomItem = arr => arr[Math.random()* arr?.length | 0 ] // 任何数按位或都是这个数的本身
+  dplayer?.current?.danmaku?.draw({
+    text:randomItem(danmu),
+    color:randomItem(colors),
+    type:randomItem(types)
+  })
+}
+
   return (
     <div className="App">
       <div className="container">
-        <div id="container"  style={{maxWidth:'550px'}}></div>
+        {loading && <div className="loading-container">
+          <Spin spinning={loading} />
+        </div>}
+        <div id="container"  style={{maxWidth:'150px'}}></div>
         <br />
 
         <Form labelCol={{ span: 6 }}
